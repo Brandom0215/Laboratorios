95acd79a39f8b4f2d2db544f6ad90f60d93b7b9c
Brandom0215 <brandom.arcia@utp.ac.pa>
2025-10-15 11:26:12 -0500

    Clase Calificacion y Interfaz hechas

---
+++ Laboratorio 2 - POO/Laboratorio 2 - POO/Clases/Calificacion.cs
@@ -0,0 +1,49 @@
+﻿
+
+public class Calificacion : IMostrable
+{
+    private Estudiante estudiante;
+    private Materia materia;
+    private double nota;
+
+    public Estudiante Estudiante
+    {
+        get => estudiante;
+        set => estudiante = value ?? throw new ArgumentException("Estudiante no puede ser nulo");
+    }
+
+    public Materia Materia
+    {
+        get => materia;
+        set => materia = value ?? throw new ArgumentException("Materia no puede ser nula");
+    }
+
+    public double Nota
+    {
+        get => nota;
+        set
+        {
+            if (value < 0 || value > 100)
+                throw new ArgumentException("La nota debe estar entre 0 y 100");
+            nota = value;
+        }
+    }
+
+    public Calificacion(Estudiante estudiante, Materia materia, double nota)
+    {
+        Estudiante = estudiante;
+        Materia = materia;
+        Nota = nota;
+    }
+
+    public double CalcularPuntos()
+    {
+        return Nota * Materia.Creditos;
+    }
+
+    public void MostrarDatos()
+    {
+        Console.WriteLine($"Calificación -> Estudiante: {Estudiante.Nombre}, Materia: {Materia.Nombre}, Nota: {Nota}");
+        Console.WriteLine("------------------------------------------");
+    }
+}
---
+++ Laboratorio 2 - POO/Laboratorio 2 - POO/Clases/IMostrable.cs
@@ -0,0 +1,5 @@
+
+public interface IMostrable
+{
+    void MostrarDatos();
+}
