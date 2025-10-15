c2f2c9186d812b0e81607ab0f44863d6a2d1022d
AnthonyPerez10 <anthony.perez10@utp.ac.pa>
2025-10-15 11:11:02 -0500

    Subiendo Clase Estudiante

---
+++ Laboratorio 2 - POO/Laboratorio 2 - POO/ClaseEstudiante.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_2___POO
+{
+    class ClaseEstudiante
+    {
+        private string nombre {  get; set; }
+        private string id { get; set; }
+        private string carrera { get; set; }
+        //private List<Calificacion> calficaciones;
+
+        //Cosntructor 
+        public ClaseEstudiante(string nombre, string id, string carrera)
+        { 
+        
+        }
+
+        public ClaseEstudiante()
+        {
+            nombre = "Juan";
+            id = "2";
+            carrera = "Licenciatura en software";
+        }
+        
+        //Metodo de calcular promedio
+        public double CalcularPromedio(float nota)
+        {
+            return 0; 
+        }
+
+    }
+}
