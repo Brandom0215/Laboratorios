2bbec2462fec4ffe9f01eaec1aae34903fe19f72
AnthonyPerez10 <anthony.perez10@utp.ac.pa>
2025-10-15 11:28:21 -0500

    Subiendo Clase Estudiante con get y set

--- Laboratorio 2 - POO/Laboratorio 2 - POO/ClaseEstudiante.cs
+++ Laboratorio 2 - POO/Laboratorio 2 - POO/ClaseEstudiante.cs
@@ -9,14 +9,33 @@ namespace Laboratorio_2___POO
 {
     class ClaseEstudiante
     {
-        private string nombre {  get; set; }
-        private string id { get; set; }
-        private string carrera { get; set; }
+        private string nombre; 
+        public string Nombre
+        {
+            get => nombre;
+            set => nombre = value ?? throw new ArgumentException("Nombre no puede ser nulo");
+        }
+        private string id; 
+        public string Id
+        {
+            get => id;
+            set => value.Trim();
+        }
+        private string carrera; 
+        public string Carrera
+        {
+            get => carrera; 
+            set => set => carrera = value ?? throw new ArgumentException("Carrera no puede ser nula");
+        }
+
         //private List<Calificacion> calficaciones;
 
         //Cosntructor 
         public ClaseEstudiante(string nombre, string id, string carrera)
-        { 
+        {
+            nombre = nombre;
+            id = id;
+            carrera = carrera; 
         
         }
 
